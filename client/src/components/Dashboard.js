de73a5d7cfdb6898e4abc3999916a691a422f141
mlampert84 <marcus.lampert@gmail.com>
2018-02-27 11:36:12 +0100

    Removing word 'dashboard' from Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,7 +5,6 @@ import SurveyList from './surveys/SurveyList';
 const Dashboard = () => {
   return (
     <div>
-      Dashboard
       <SurveyList />
       <div className="fixed-action-btn">
         <Link to="/surveys/new" className="btn-floating btn-large red">
